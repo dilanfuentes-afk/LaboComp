6dd5000dd25d565014f6347c52a1f911f91a6f3e
Dilan Fuentes <dilanfuentes37@gmail.com>
2025-07-27 03:16:42 -0300

    Ejercicio21

---
+++ unidad-2/src/Ejercicio21.java
@@ -0,0 +1,8 @@
+public class Ejercicio21 {
+    public static void main(String[] args) {
+        Holamundo();
+    }
+    public static void Holamundo() {{
+        System.out.println("Hola, mundo");
+    }}
+}
