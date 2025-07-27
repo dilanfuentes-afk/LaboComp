b778e2798a99b5162675ebe992c1319f00c525a1
dilanfuentes-afk <dilanfuentes37@gmail.com>
2025-07-27 02:33:05 -0300

    Add files via upload

---
+++ Repo/unidad-2/src/Ejercicio6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+                int num1;
+                int num2;
+
+        System.out.print("Ingrese el primer numero: ");
+        num1 = ent.nextInt();
+        System.out.print("Ingrese el segundo numero: ");
+        num2 = ent.nextInt();
+
+        if (num2 != 0){
+            num2 /= num1;
+            System.out.println("El cociente guardado en num2 es " + num2);
+        } else {
+                System.out.println("No se puede dividir entre 0");
+            }
+    }
+}
